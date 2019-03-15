839cf63038478174e3818534eeb40069c63d130f
rgoel2 <rohangoel31@gmail.com>
2019-03-15 00:45:12 -0700

    changes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist/llc')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/dist/llc/index.html'));
