c3d5999a7d5a4096884d1128f9aff79531c34d65
rgoel2 <rohangoel31@gmail.com>
2019-03-14 23:25:36 -0700

    environmet.prod.ts changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDFM_8C5mntY0ElJ-quIf4hePkWPZu4ud0",
+    authDomain: "mcop-65e2a.firebaseapp.com",
+    databaseURL: "https://mcop-65e2a.firebaseio.com",
+    projectId: "mcop-65e2a",
+    storageBucket: "mcop-65e2a.appspot.com",
+    messagingSenderId: "219941573952"
+  }
 };
